6e6d32c36a305ae25a915f3340ebcf78a13dda79
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-02 14:10:48 -1000

    created server

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const net = require('net');
 const http = require('http');
 const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  server.on('')
+
+  // server.on('request' (request) => {
+  //   let message = request.split("\n\n");
+  //   let header = message[0];
+  //   let body = message[2];
+  // })
+})
+
+server.listen(8080, () => {
+  console.log('listening on port 8080');
+});
