7f4968b7c8e2d2b4ff2188deb6a93f454d3e9589
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-02 16:05:53 -1000

    bare bones functionality

--- server.js
+++ server.js
@@ -1,17 +1,67 @@
 const net = require('net');
-const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  server.on('')
+const helium = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - Helium</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>Helium</h1>
+  <h2>H</h2>
+  <h3>Atomic number 2</h3>
+  <p>Helium is a chemical element with symbol He and atomic number 2. It is a colorless, odorless, tasteless, non-toxic, inert, monatomic gas that heads the noble gas group in the periodic table. Its boiling and melting points are the lowest among all the elements and it exists only as a gas except in extremely cold conditions.</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>`
 
-  // server.on('request' (request) => {
-  //   let message = request.split("\n\n");
-  //   let header = message[0];
-  //   let body = message[2];
-  // })
-})
+const hydrogen = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - Hydrogen</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>Hydrogen</h1>
+  <h2>H</h2>
+  <h3>Atomic number 1</h3>
+  <p>Hydrogen is a chemical element with chemical symbol H and atomic number 1. With an atomic weight of 1.00794 u, hydrogen is the lightest element on the periodic table. Its monatomic form (H) is the most abundant chemical substance in the universe, constituting roughly 75% of all baryonic mass. Non-remnant stars are mainly composed of hydrogen in its plasma state. The most common isotope of hydrogen, termed protium (name rarely used, symbol 1H), has a single proton and zero neutrons.</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>`
+
+let contentArray = [hydrogen, helium];
+
+const server = net.createServer((request) => {
+
+  request.on('data', (data) => {
+    // console.log(data);
+    let message = data.toString().split("\r\n");
+    console.log(message);
+
+    let reqLine = message[0].split(" ");
+    console.log(reqLine);
+    let method = reqLine[0];
+    let reqURI = reqLine[1];
+    console.log(reqURI);
+    let httpV = reqLine[2];
+
+    makeResponse(method, reqURI);
+
+  });
+});
 
 server.listen(8080, () => {
   console.log('listening on port 8080');
 });
+
+function makeResponse(meth, uri){
+  contentArray.forEach((element) => {
+    if(uri === (`/${element}.html`)){
+      request.write(element);
+    }
+  })
+}
