e7ce4f4cb18a0dfa944b331a4b5285dae2e30337
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-03 15:58:55 -1000

    basic functionality working

--- client.js
+++ client.js
@@ -12,14 +12,23 @@ server.connect(8080, '0.0.0.0', () => {
     let host = initialReq[0].split("/")[0];
     let uri = initialReq[0].split("/")[1];
     let newReq = buildRequest("GET", host, uri);
-    server.write(newReq);
-    server.pipe(process.stdout);
+    server.write(newReq, () => {
+      server.on('data', (data) => {
+        let response = data.toString();
+        let body = response.split(/\n\s*\n/);
+        process.stdout.write(body[1]);
+      })
+    });
+    // server.pipe(process.stdout);
   }else if(initialReq.length === 2 && initialReq[0] === "-I"){
     let host = initialReq[1].split("/")[0];
     let uri = initialReq[1].split("/")[1];
     let newReq = buildRequest("HEAD", host, uri);
     server.write(newReq);
     server.pipe(process.stdout);
+  }else if(initialReq){
+    console.log("You gotta give some to get some.");
+    server.end();
   }
 
 });
--- server.js
+++ server.js
@@ -2,43 +2,22 @@ const net = require('net');
 const fs = require('fs');
 const path = require('path');
 
-// let filePath = path.join(__dirname, '..', 'public' 'test.txt');
-// console.log(filePath);
-
-// let fileContents;
-
-// fs.readFile(filePath, (err, data) => {
-
-// })
-
-// console.log(fileContents);
-
-let content = {"./hydrogen.html": "hydrogen", "./helium.html": "helium", "./index.html": "index", "./css/styles.css": "css"};
+const content = {"./hydrogen.html": "hydrogen", "./helium.html": "helium", "./index.html": "index", "./css/styles.css": "css"};
 
 const server = net.createServer((request) => {
 
-  // server.getConnections(function(err, count){
-  //   request.write('What is your request?\n');
-  // });
-
   request.on('data', (data) => {
-    // console.log(data);
     let message = data.toString().split("\r\n");
-    // console.log(message);
-
     let reqLine = message[0].split(" ");
-    // console.log(reqLine);
     let method = reqLine[0];
     let reqURI = `.${reqLine[1]}`;
-    console.log(reqURI);
     let httpV = reqLine[2];
-
-    // makeResponse(reqURI);
     let date = new Date().toString();
     let element = null;
     let status = null;
     let length = null;
     let count = 0;
+
     for(let key in content){
       count++;
       if(reqURI === key){
@@ -52,11 +31,18 @@ Server: MyServer
 Date: ${date}
 Content-Type: text/html; charset=utf-8
 Content-Length: ${length}
-Connection: keep-alive\n`
+Connection: keep-alive\n\n`;
           let body = `${element}\n`;
-          // console.log(output);
+          let both = `HTTP/1.1 ${status}
+Server: MyServer
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${length}
+Connection: keep-alive
+
+${element}\n`
           if(method === "GET"){
-            request.write(body, (err) => {
+            request.write(both, (err) => {
               request.end();
             });
           }else if(method === "HEAD"){
@@ -78,7 +64,6 @@ Content-Length: ${length}
 Connection: keep-alive
 
 ${element}\n`;
-          // console.log(output);
           request.write(output, (err) => {
             request.end();
           });
@@ -98,63 +83,18 @@ server.listen(8080, () => {
 //   let reqLine = message[0].split(" ");
 //   let method = reqLine[0];
 //   let reqURI = `.${reqLine[1]}`;
+// }
 
+// function makeReq(method, uri){
+//   if(method === "GET"){
+
+//   }
 // }
 
 // function read(element, cb){
 //   let filePath = path.join(__dirname, element);
 //   fs.readFile(filePath, 'uft8', (err, data) => {
 //     if (err) { return cb(err); }
-
-// function makeResponse(method, uri, cb){
-//   let date = new Date().toString();
-//   let element;
-//   let status;
-//   let length;
-//   let count = 0;
-
-//   for(let key in content){
-//     count++;
-//     if(reqURI === key){
-//       status = "200 OK"
-//       return fs.readFile(key, 'utf8', (err, data) => {
-//         if (err) throw err;
-//         element = data;
-//         length = data.length;
-//         let output = `HTTP/1.1 ${status}
-// Server: MyServer
-// Date: ${date}
-// Content-Type: text/html; charset=utf-8
-// Content-Length: ${length}
-// Connection: keep-alive
-
-// ${element}`;
-//         console.log(output);
-//         request.write(output, (err) => {
-//           request.end();
-//         });
-//       });
-//     }else if(reqURI !== key && count === content.length){
-//       status = "404 Not Found"
-//       return fs.readFile('./404.html', 'utf8', (err, data) => {
-//         element = data;
-//         length = data.length;
-//         let output1 = `HTTP/1.1 ${status}
-// Server: MyServer
-// Date: ${date}
-// Content-Type: text/html; charset=utf-8
-// Content-Length: ${length}
-// Connection: keep-alive
-
-// ${element}`;
-//         console.log(output1);
-//         request.write(output1, (err) => {
-//           request.end();
-//         });
-//       });
-//     }
-//   }
-// }
 
 // function write(output){
 
